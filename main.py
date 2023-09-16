e28982dec40fb1aa3ea4549a7648b8aa56f88355
unknown <nguyenhoangkhang12102003@gmail.com>
2023-09-16 12:33:23 +0700

    create CRUD

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
+from typing import Optional
+from random import randrange
 
 class Post(BaseModel):
     title: str
     content: str
     published: bool = True
+    rating: Optional[int] = None
+
+my_posts = [{"id": 111111}]
+
+def find_post(id):
+    for post in my_posts:
+        if post['id'] == id:
+            return post
 
 app = FastAPI()
 
@@ -15,8 +25,25 @@ async def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your posts"}
+    return {"data":my_posts}
 
-@app.post("/createposts")
-def create_posts(new_post: Post):
-    print(new_post)
+@app.post("/posts")
+def create_posts(post: Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0,1000000)
+    print(type(post_dict['id']) )
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
+@app.get("/posts/latest")
+def get_latest_post():
+    post = my_posts[-1]
+    return {"detail": post}
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with id {id} was not found")
+    return {"post_detail": post}
