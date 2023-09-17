d5b199e20f1d02cd1b7fa6f015f0ed4abe0e9e0e
unknown <nguyenhoangkhang12102003@gmail.com>
2023-09-17 18:26:07 +0700

    add function user login

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ import time
 from . import models, schemas, utils
 from .database import engine, get_db
 from sqlalchemy.orm import Session
-from .routers import post, user
+from .routers import post, user, auth
 models.Base.metadata.create_all(bind=engine)
 
 while True:
@@ -26,6 +26,7 @@ app = FastAPI()
 
 app.include_router(post.router)
 app.include_router(user.router)
+app.include_router(auth.router)
 
 @app.get("/")
 async def root():
