13c639b19db3e36690d0f120e3c4c9f3d44b17d9
unknown <nguyenhoangkhang12102003@gmail.com>
2023-09-24 12:25:30 +0700

    test login function

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,7 +1,7 @@
 from app import schemas
-from .database import client, session
+from jose import jwt
+from app.config import settings
 import pytest
-
 
 # def test_root(client):
 #     res = client.get("/")
@@ -9,25 +9,35 @@ import pytest
 #     assert res.json().get("message") == "Hello world"
 #     assert res.status_code == 200
 
-@pytest.fixture
-def test_user(client):
-    user_data = {"email":"khang@gmail.com",
-                 "password": "khang1210"}
-    res = client.post("/users", json=user_data)
-    assert res.status_code == 201
-    new_user = res.json()
-    new_user["password"] = user_data["password"]
-    return new_user
+
 
 def test_create_user(client):
-    res = client.post("/users", json={"email": "hello@gmail.com",
+    res = client.post("/users", json={"email": "khang@gmail.com",
                                       "password": "khang1210"})
     new_user = schemas.UserOut(**res.json())
-    assert new_user.email == "hello@gmail.com"
+    assert new_user.email == "khang@gmail.com"
     assert res.status_code == 201
 
 def test_user_login(test_user, client):
     res = client.post("/login", data = {"username": test_user["email"],
                                         "password": test_user["password"]})
-    
+    login_res = schemas.Token(**res.json())
+    payload = jwt.decode(login_res.access_token, settings.secret_key, algorithms=[settings.algorithm])
+    id = payload.get("user_id")
+    assert id == test_user['id']
+    assert login_res.token_type == 'bearer'
     assert res.status_code == 200
+
+@pytest.mark.parametrize("email, password, status_code", [
+    ("wrong@gmail.com", "khang1210", 403),
+    ("khang@gmail.com", "wrong", 403),
+    ("wrong@gmail.com", "wrong", 403),
+    (None, "khang1210", 422),
+    ("khang@gmail.com", None, 422)
+])
+
+def test_incorrect_login(test_user, client, email, password, status_code):
+    res = client.post("/login", data={"username": email, "password": password})
+
+    assert res.status_code == status_code
+    #assert res.json().get("detail") == "Invalid Credentials"
